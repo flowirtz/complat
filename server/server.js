068c9c812aa23b1ead672bbc7d3392052a8d101b
Marius Danner <marius.danner@student.hpi.de>
2017-03-06 18:08:26 +0100

    new shit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var ws = require("nodejs-websocket");
 var fs = require("fs");
 var problemList=[];
+var userdict;
+var exec = require('child_process').exec;
+
 var server = ws.createServer(function (conn) {
   console.log("New Con");
   conn.on("text", function (str) {
@@ -9,10 +12,12 @@ var server = ws.createServer(function (conn) {
     if (obj.type == "problem") {
       problemList.push(obj);
       problemList[problemList.length - 1].problemID = problemList.length - 1;
-      //notify...
+      exec('phonegap push --deviceID fhHuY121FMY:APA91bHjBk_5U0bWe7u4nZDMKhVv8px30u1KZWxJ71xol9lGXhveHoWlprCqfc9DVrtnUsRlPB9lV3GPll_4UsuIw5jcnzP17ln5vZY7zryuEgJZnd2qq8XRPJY6GLBlrMKkRem2hPAx --service gcm --payload \'{ "data": { "title": "Problem with machine"' + obj.machineID + ', "message": '+ obj.description +'}\'');
+      console.log("notified a receiver");
     } else if (obj.type == "solution") {
-      problemList[obj.problemID].solved = true;
+      problemList[obj.problemID].solved = obj.submitterID;
       problemList[obj.problemID].solution = obj.solution;
+
     } else if (obj.type == "list_request") {
       console.log(problemList)
       conn.sendText(JSON.stringify({list:problemList}));
